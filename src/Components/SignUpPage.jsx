b75e01c3d75d524bc6f310e21178289124aa989b
Anurag <anuktsksanurag@gmail.com>
2025-05-23 12:33:46 +0530

    fixed the apis

--- src/Components/SignUpPage.jsx
+++ src/Components/SignUpPage.jsx
@@ -34,7 +34,7 @@ const SignUpForm = () => {
     setLoading(true);
     
     try {
-      const response = await fetch('http://localhost:5000/api/auth/signup', {
+      const response = await fetch('https://yusuf.pollai.in/api/auth/signup', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
